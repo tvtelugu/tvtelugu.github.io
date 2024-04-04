cddce61bc0301a34c2961f6b5bd1983f80ab9bc2
TVTelugu <63017421+tvtelugu@users.noreply.github.com>
2024-04-04 19:06:29 +0530

    Create script.js

---
+++ Live/ipl/script.js
@@ -0,0 +1,39 @@
+function myFunction() {
+          if (Hls.isSupported()) {
+            var video = document.getElementById('video');
+            var hls = new Hls();
+            var url = document.getElementById("videoselection").value;
+
+            // bind them together
+            hls.attachMedia(video);
+            hls.on(Hls.Events.MEDIA_ATTACHED, function() {
+              console.log("video and hls.js are now bound together !");
+              hls.loadSource(url);
+              hls.on(Hls.Events.MANIFEST_PARSED, function(event, data) {
+                video.play();
+                console.log("manifest loaded, found " + data.levels.length + " quality level");
+
+              });
+            });
+          }
+        }
+
+        function myFunction1() {
+          if (Hls.isSupported()) {
+            var video = document.getElementById('video');
+            var hls = new Hls();
+            var url = document.getElementById("textbox").value;
+
+            // bind them together
+            hls.attachMedia(video);
+            hls.on(Hls.Events.MEDIA_ATTACHED, function() {
+              console.log("video and hls.js are now bound together !");
+              hls.loadSource(url);
+              hls.on(Hls.Events.MANIFEST_PARSED, function(event, data) {
+                video.play();
+                console.log("manifest loaded, found " + data.levels.length + " quality level");
+
+              });
+            });
+          }
+        }
