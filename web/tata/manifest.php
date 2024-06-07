8a077dcbd1805c4586907a3ba6c55e456b5e5a83
TVTelugu <63017421+tvtelugu@users.noreply.github.com>
2024-06-07 21:36:24 +0530

    Add files via upload

---
+++ web/tata/manifest.php
@@ -0,0 +1,40 @@
+<?php
+
+$fetcherJson = file_get_contents('bpprod.json');
+$fetcherData = json_decode($fetcherJson, true);
+$id = $_GET['id'] ?? null;
+
+if ($id === null) {
+  echo '<h1>Error: No Channel ID provided</h1>';
+  exit;
+}
+$channelData = null;
+foreach ($fetcherData['data']['channels'] as $channel) {
+  if ($channel['id'] === $id) {
+    $channelData = $channel;
+    break;
+  }
+}
+
+if ($channelData === null) {
+  echo '<h1>Error: Channel not found</h1>';
+  exit;
+}
+
+$currentTimestamp = time();
+
+$beginTimestamp = $currentTimestamp - (8 * 24 * 60 * 60);
+
+
+$endTimestamp = $currentTimestamp + (2 * 24 * 60 * 60);
+
+$beginFormatted = gmdate('Ymd\THis', $beginTimestamp);
+$endFormatted = gmdate('Ymd\THis', $endTimestamp);
+
+$manifestUrl = $channelData['manifest_url'];
+
+$time = '?begin=' . $beginFormatted . '&end=' . $endFormatted;
+
+$mpdurl = $manifestUrl.$time;
+header("Location: $mpdurl")
+?>
