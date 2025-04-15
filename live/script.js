e3b811fe9284eff1d3aec90db4fcd7ee6ee85fe6
TVTelugu <63017421+tvtelugu@users.noreply.github.com>
2025-04-15 21:21:47 +0530

    Update script.js

--- live/script.js
+++ live/script.js
@@ -1,5 +1,5 @@
     
-        fetch('https://raw.githubusercontent.com/tvtelugu/tvtelugu.github.io/refs/heads/main/live/scroll1.json')
+        fetch('https://news.google.com/rss?hl=te&gl=IN&ceid=IN:te')
             .then(response => response.text())
             .then(data => {
                 document.getElementById('marquee').innerHTML = data;
